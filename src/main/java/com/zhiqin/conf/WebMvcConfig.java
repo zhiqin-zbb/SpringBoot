99934b1b4c599410c45f4ec8b4276735b9f85220
zhiqin-zbb <451053802@qq.com>
2017-08-26 18:08:33 +0800

    配置Json格式入参解析

--- src/main/java/com/zhiqin/conf/WebMvcConfig.java
+++ src/main/java/com/zhiqin/conf/WebMvcConfig.java
@@ -1,8 +1,13 @@
 package com.zhiqin.conf;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.zhiqin.util.annotation.JsonArgumentResolver;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -10,5 +15,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new JsonArgumentResolver());
     }
 }
