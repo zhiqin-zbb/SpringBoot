2d3eba17ff9d2945c21e308515570b845781d492
zhiqin-zbb <451053802@qq.com>
2017-08-28 11:24:16 +0800

    兼容内置与外置Tomcat

--- src/main/java/com/zhiqin/Application.java
+++ src/main/java/com/zhiqin/Application.java
@@ -3,18 +3,24 @@ package com.zhiqin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Controller
 @EnableWebMvc
 @SpringBootApplication
 @MapperScan(basePackages = "com.zhiqin.mapper")
-public class Application extends WebMvcConfigurationSupport {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
     @RequestMapping("/")
