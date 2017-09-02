2284c9aceee5a678799e2b0fa1ae2d987be0d15c
zhiqin-zbb <451053802@qq.com>
2017-09-02 09:37:45 +0800

    @Data

--- src/main/java/com/zhiqin/model/BaseEntity.java
+++ src/main/java/com/zhiqin/model/BaseEntity.java
@@ -1,11 +1,14 @@
 package com.zhiqin.model;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+@Data
 public class BaseEntity {
     @Id
     @Column(name = "Id")
@@ -17,28 +20,4 @@ public class BaseEntity {
 
     @Transient
     private Integer rows = 10;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
-    }
-
-    public Integer getRows() {
-        return rows;
-    }
-
-    public void setRows(Integer rows) {
-        this.rows = rows;
-    }
 }
