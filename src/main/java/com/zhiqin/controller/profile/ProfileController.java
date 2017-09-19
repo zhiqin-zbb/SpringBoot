154be1b07063cc57f6b06800f972fa05f07fc916
zhiqin-zbb <451053802@qq.com>
2017-09-19 10:39:22 +0800

    profile页面

--- src/main/java/com/zhiqin/controller/profile/ProfileController.java
+++ src/main/java/com/zhiqin/controller/profile/ProfileController.java
@@ -1,5 +1,9 @@
 package com.zhiqin.controller.profile;
 
+import java.util.Locale;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,7 +11,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.druid.support.json.JSONUtils;
+import com.zhiqin.common.date.LocalDateFormatter;
+import com.zhiqin.common.util.json.JsonUtil;
 import com.zhiqin.model.profile.ProfileForm;
 
 /**
@@ -17,14 +25,25 @@ import com.zhiqin.model.profile.ProfileForm;
 public class ProfileController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return LocalDateFormatter.getPattern(locale);
+    }
+
     @RequestMapping("/profile")
-    public String displayProfile() {
-        return "profile/profilePage";
+    public ModelAndView displayProfile() {
+        ModelAndView result = new ModelAndView("profile/profilePage");
+        result.addObject("profileForm", new ProfileForm());
+        return result;
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(@ModelAttribute("profileForm") ProfileForm profileForm, BindingResult result) {
-        logger.info("save ok" + profileForm);
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile/profilePage";
+        }
+
+        logger.info("save ok:[{}]", JsonUtil.toString(profileForm));
         return "redirect:/profile";
     }
 }
--- src/main/java/com/zhiqin/model/profile/ProfileForm.java
+++ src/main/java/com/zhiqin/model/profile/ProfileForm.java
@@ -4,6 +4,14 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.zhiqin.common.date.PastLocalDate;
+
 import lombok.Data;
 
 /**
@@ -11,10 +19,15 @@ import lombok.Data;
  */
 @Data
 public class ProfileForm {
+    @Size(min = 2)
     private String twitterHandle;
 
+    @Email
+    @NotEmpty
     private String email;
 
+    @NotNull
+    @PastLocalDate
     private LocalDate birthDate;
 
     private List<String> tastes = new ArrayList<>();
